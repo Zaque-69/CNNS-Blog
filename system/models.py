c3d391dea13d44ddcff1c43da79b5a105eb46fc1
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-09-13 22:26:39 +0300

    Add files via upload

---
+++ system/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Profile:
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profilePics')
+
+    def __str__(self) :
+        return f'{self.user.username} Profie'
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
---
+++ system/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from .models import Post
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def home(request):
+    context = {
+        'posts' : Post.objects.all()
+    }
+    return render(request, 'home.html', context)
+
+def register(request):
+    form = UserRegistrationForm()
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    return render(request, 'register.html', {
+        'form' : form
+        })
+
+def loginSystem(request):
+    if request.method == 'POST':
+        usern = request.POST.get('username')
+        passw = request.POST.get('password1')
+        user = authenticate(request, username = usern, password = passw)
+
+        if user is not None :
+            login(request, user)
+            return redirect('home')
+        else : print('muci')
+
+    return render(request, 'login.html')
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
