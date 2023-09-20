34bcb68acbb296409bb62feffbcfb16aca8a7f94
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-09-20 13:51:52 -0700

    Update forms.py

--- system/forms.py
+++ system/forms.py
@@ -8,3 +8,10 @@ class UserRegistrationForm(UserCreationForm):
     class Meta :
         model = User
         fields = ['username', 'password1', 'password2', 'email']
+
+class UpdateUser(forms.ModelForm):
+    
+    email = forms.EmailField()
+    class Meta : 
+        model = User
+        fields = ['username', 'email']
