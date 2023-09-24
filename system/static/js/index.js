32de4dc789d938d0af5eb0a0a12c5018c3aad167
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-09-24 11:38:08 -0700

    Update index.js

--- system/static/js/index.js
+++ system/static/js/index.js
@@ -1,4 +1,7 @@
-console.log(1);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function changeThemes(col1, col2, col3, col4, col5){
     $('nav').css('background-color', col1);
     $('body').css('background-color', col2);
@@ -7,5 +10,20 @@ function changeThemes(col1, col2, col3, col4, col5){
     
     const changeTextColor = ['h1', 'h2', 'p'];
     for(let i = 0; i < changeTextColor.length; i++) $(changeTextColor[i]).css('color', col5);
-    // #02113D
 }
+
+$('.fullscreen').css('height', window.innerHeight + 'px');
+
+setTimeout(() => $('.fullscreen').fadeOut(500), 2000)
+var count = 0; 
+
+function sleepText(cls){
+    $( cls ).each(function() {
+        $(this).fadeIn(count * 200);
+        count ++ ;
+    });
+}
+sleepText('.CNNSletters');
+sleepText('.PL');
+sleepText('.Home');
+
