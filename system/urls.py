2f5c7cd2f38dfdc01b2b2752c26b20f8601833e3
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-09-26 00:00:53 -0700

    Update urls.py

--- system/urls.py
+++ system/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostdDetailView, PostCreateView, PostUpdateView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
+    path('', PostListView.as_view(), name = 'home'),
+    path('error/', views.error, name = 'errorLog'),
+    path('post/new/', PostCreateView.as_view(), name = 'post-new'),
     path('register/', views.register, name = 'register'),
     path('login/', views.loginSystem, name = 'log'),
+    path('profile/',  views.profile , name = 'profile'),
+    path('post/<pk>/', PostdDetailView.as_view(), name = 'post-detail'),
+    path('post/<pk>/update', PostUpdateView.as_view(), name = 'post-update'),
+    #path('post/<pk>/delete', PostdeleteView.as_view(), name = 'post-delete'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'logout.html') , name = 'logout'),
-    path('profile/',  views.profile , name = 'profile')
 ]
