b888d74990c9500e22b86b0f3303828a8a2384a3
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-09-26 00:01:04 -0700

    Update views.py

--- system/views.py
+++ system/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import UserRegistrationForm, UpdateUser
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DeleteView, CreateView, UpdateView, DetailView
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 
@@ -11,14 +13,60 @@ def home(request):
     }
     return render(request, 'home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostdDetailView(DeleteView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'postNew.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form): 
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'postNew.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author : return True
+        else : return False
+
+'''class PostdeleteView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    model = Post
+    template_name = 'postDelete.html'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author : return True
+        else : return False'''
+
+def error(request) : return render(request, '404.html')
+
 def register(request):
     form = UserRegistrationForm()
 
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
+
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('log')
+        else : return redirect('errorLog')
 
     return render(request, 'register.html', {
         'form' : form
@@ -30,11 +78,9 @@ def loginSystem(request):
         passw = request.POST.get('password1')
         user = authenticate(request, username = usern, password = passw)
 
-        #try:
         if user is not None : 
             login(request, user)
             return redirect('home')
-        #except AttributeError : print('ma-ta')
 
     return render(request, 'login.html')
 
@@ -52,3 +98,10 @@ def profile(request):
         'u_form' : u_profile,
     }
     return render(request, 'profile.html', context)
+
+'''def delete_post(request, id):
+    post_to_delete=Post.objects.get(id = object.id)
+    post_to_delete.delete()'''
+
+def StatusCode404(request, exception):
+    return render(request, '404.html')
