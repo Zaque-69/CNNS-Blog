348459657455b537f47ce127a824da29ee021752
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-10-03 09:32:00 +0300

    Update views.py

--- system/views.py
+++ system/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import UserRegistrationForm, UpdateUser
+import json
 from .models import Post
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm, UpdateUser, UpdateUserAfterClass
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DeleteView, CreateView, UpdateView, DetailView
 from django.contrib.auth import login, authenticate
@@ -8,10 +9,96 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
-    context = {
-        'posts' : Post.objects.all()
-    }
-    return render(request, 'home.html', context)
+    return render(request, 'home.html', {
+        'posts' : Post.objects.all().order_by('-date_posted')
+    })
+
+def clasaX(request):
+    return render(request, 'clasaX.html', {
+        'posts' : Post.objects.all().order_by('-date_posted')
+    })
+
+def error(request) : 
+    return render(request, '404.html')
+
+def register(request):
+    form = UserRegistrationForm()
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+
+        form.save()
+        usern = request.POST.get('username')
+        passw = request.POST.get('password1')
+        user = authenticate(request, username = usern, password = passw)
+        login(request, user)
+        return redirect('ClassRoom')
+    
+    return render(request, 'register.html', {
+        'form' : form
+        })
+
+def loginSystem(request):
+    if request.method == 'POST':
+        usern = request.POST.get('username')
+        passw = request.POST.get('password1')
+        user = authenticate(request, username = usern, password = passw)
+
+        if user is not None : 
+            login(request, user)
+            return redirect('home')
+        #else : messages.info(request, "Three credits remain in your account.")
+
+    return render(request, 'login.html')
+
+
+def SelectClass(request):
+    form = UpdateUserAfterClass()
+    listOfCodes = ["HKKnGCU3LJv5f3rd1TUrPQ==", "JahLB6ny+in3NYZq/6qyGQ==",
+                       "cc5MScnZUesxuthyWo52iQ==", "pbWhFEPPPbjWSxoLpYhSqg=="]
+    if request.method == 'POST':
+        ok = 0
+        form = UpdateUserAfterClass(request.POST, instance = request.user)
+        for code in listOfCodes :
+            requestInvitation = request.POST.get('last_name')
+            if code == requestInvitation : ok = 1
+
+        if ok == 1 : 
+            print(json.loads)
+            if form.is_valid() : 
+                with open("./system/static/js/main.json", "r") as f: 
+                    requestInvitation = json.load(f)[requestInvitation]
+                print(requestInvitation)
+                form.save()
+            return redirect('home')
+        #else : messages.info(request, "Three credits remain in your account.")
+
+    return render(request, 'classroom.html', {
+        'form' : form
+    })
+
+@login_required
+def profile(request):
+    u_profile = UpdateUser()
+    if request.method == 'POST' :
+        u_profile = UpdateUser(request.POST, instance = request.user)
+        if u_profile.is_valid() : 
+            u_profile.save()
+    return render(request, 'profile.html', { 'u_form' : u_profile,})
+
+
+def StatusCode404(request, exception):
+    return render(request, '404.html')
+
+@login_required
+def contactPage(request) : 
+    return render(request, 'contact.html')
+
+@login_required
+def anunturiPage(request) : 
+    return render(request, 'anunturi.html', {
+        'posts' : Post.objects.all().order_by('-date_posted')
+    })
 
 class PostListView(ListView):
     model = Post
@@ -19,14 +106,14 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
-class PostdDetailView(DeleteView):
+class PostdDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
 class PostCreateView(CreateView):
     model = Post
     template_name = 'postNew.html'
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'class4post']
 
     def form_valid(self, form): 
         form.instance.author = self.request.user
@@ -46,65 +133,12 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
         if self.request.user == post.author : return True
         else : return False
 
-'''class PostdeleteView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+class PostdeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'postDelete.html'
+    success_url = '/'
 
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author : return True
-        else : return False'''
-
-def error(request) : return render(request, '404.html')
-
-def register(request):
-    form = UserRegistrationForm()
-
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return redirect('log')
-        else : return redirect('errorLog')
-
-    return render(request, 'register.html', {
-        'form' : form
-        })
-
-def loginSystem(request):
-    if request.method == 'POST':
-        usern = request.POST.get('username')
-        passw = request.POST.get('password1')
-        user = authenticate(request, username = usern, password = passw)
-
-        if user is not None : 
-            login(request, user)
-            return redirect('home')
-
-    return render(request, 'login.html')
-
-@login_required
-def profile(request):
-    u_profile = UpdateUser()
-
-    if request.method == 'POST' :
-        u_profile = UpdateUser(request.POST, instance = request.user)
-        
-        if u_profile.is_valid() : 
-            u_profile.save()
-
-    context = {
-        'u_form' : u_profile,
-    }
-    return render(request, 'profile.html', context)
-
-'''def delete_post(request, id):
-    post_to_delete=Post.objects.get(id = object.id)
-    post_to_delete.delete()'''
-
-def StatusCode404(request, exception):
-    return render(request, '404.html')
-
-@login_required
-def contactPage(request) : return render(request, 'contact.html')
+        else : return False
