161e1557aad22569089424038c51c51f0c002e48
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-10-06 09:37:37 +0300

    Update models.py

--- system/models.py
+++ system/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    class4post = models.CharField(max_length=100)
+    class4post = models.CharField(max_length=30)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -14,3 +14,14 @@ class Post(models.Model):
         return self.title
     
     def get_absolute_url(self) : return reverse('post-detail', kwargs = {'pk' : self.pk})
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name = 'comments', on_delete=models.CASCADE)
+    body = models.TextField()
+    class4post = models.CharField(max_length=100)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) : return self.post.title
+
+    def get_absolute_url(self) : return reverse('comments-detail', kwargs = {'pk' : self.pk})
