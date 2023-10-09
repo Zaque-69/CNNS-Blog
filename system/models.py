36a333f47e026369057dde93126ff7a559cb4a87
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-10-10 00:00:41 +0300

    Update models.py

--- system/models.py
+++ system/models.py
@@ -25,3 +25,14 @@ class Comment(models.Model):
     def __str__(self) : return self.post.title
 
     def get_absolute_url(self) : return reverse('comments-detail', kwargs = {'pk' : self.pk})
+
+class ImageModel(models.Model):
+    title = models.CharField(max_length=255)
+    class4post = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='profilePics')
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self) : return self.title
+
+    def get_absolute_url(self) : return reverse('image-detail', kwargs = {'pk' : self.pk})
