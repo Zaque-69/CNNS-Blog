51306b712630aec448b6eeb7e4aef8d10497d418
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-10-12 23:51:30 +0300

    Update urls.py

--- system/urls.py
+++ system/urls.py
@@ -3,32 +3,26 @@ from . import views
 from .views import (PostdDetailView, PostCreateView,
                     PostUpdateView, PostdeleteView,
                     PostUpdateViewComment, PostdeleteViewComment,
-                    PostdDetailViewComment,CommentCreateView, 
-                    ImageCreateView,ImageUpdateView, 
-                    ImageDeleteView, ImageDetailView)
+                    PostdDetailViewComment,CommentCreateView)
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('error/', views.error, name = 'errorLog'),
+    path('post/new/', PostCreateView.as_view(), name = 'post-new'),
+    path('comment/new/', CommentCreateView.as_view(), name = 'comment-new'),
+
     path('register/', views.register, name = 'register'),
     path('login/', views.loginSystem, name = 'log'),
     path('profile/',  views.profile , name = 'profile'),
 
-    path('post/new/', PostCreateView.as_view(), name = 'post-new'),
     path('post/<pk>/', PostdDetailView.as_view(), name = 'post-detail'),
     path('post/<pk>/update/', PostUpdateView.as_view(), name = 'post-update'),
     path('post/<pk>/delete/', PostdeleteView.as_view(), name = 'post-delete'),
 
-    path('comment/new/', CommentCreateView.as_view(), name = 'comment-new'),
     path('comment/<pk>/', PostdDetailViewComment.as_view(), name = 'comments-detail'),
     path('comment/<pk>/update/', PostUpdateViewComment.as_view(), name = 'comments-update'),
     path('comment/<pk>/delete/', PostdeleteViewComment.as_view(), name = 'comments-delete'),
-
-    path('image/new/', ImageCreateView.as_view(), name = 'image-new'),
-    path('image/<pk>/', ImageDetailView.as_view(), name = 'image-detail'),
-    path('image/<pk>/update/', ImageUpdateView.as_view(), name = 'image-update'),
-    path('image/<pk>/delete/', ImageDeleteView.as_view(), name = 'image-delete'),
 
     path('logout/', auth_views.LogoutView.as_view(template_name = 'logout.html') , name = 'logout'),
     path('contact/', views.contactPage, name = 'contactPage'),
