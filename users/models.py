a4e2b207b94f1265e90d4f9e4b02e80ad0a0c8e0
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-10-14 00:12:21 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+import random
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profilePics')
+    image = models.ImageField(default = random.choice(['default1.png', 'default2.png', 'default3.png', 'default4.png', 'default5.png']), upload_to='profilePics')
 
-    def __str__(self) : return f'{self.user.username} profile'
+    def __str__(self) : return f'{self.user.username} profile' 
