be321237a7eeac771aaf6820775ab4ff0cef09d1
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-10-14 00:15:22 +0300

    Update models.py

--- system/models.py
+++ system/models.py
@@ -22,6 +22,7 @@ class Comment(models.Model):
     class4post = models.CharField(max_length=100)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True, upload_to='commentsImages')
 
     def __str__(self) : return self.post.title
 
