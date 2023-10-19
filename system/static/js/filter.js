e15710ff1436719a417556673fd69da44c8083a8
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-10-20 00:09:55 +0300

    Update filter.js

--- system/static/js/filter.js
+++ system/static/js/filter.js
@@ -1,1 +1,10 @@
-
+const listOfClasses = ['mxWLOPyckongDCJGBpQK', 'eRstQPMlfBoYsLBVZgMW', 'QFdINEQehJkVrcHZtUJs', 'amcYvyDjbIjNAWyLEwJG', 'anunt'];
+document.getElementById('filterButton').addEventListener('click', function(){
+    for (var i = 0; i < listOfClasses.length; i++) {
+        document.querySelectorAll('.' + listOfClasses[i]).forEach((e) =>{
+            if ( listOfClasses[i] == document.querySelector('select').value ) e.style.display = 'block';
+            else e.style.display = 'none';
+        })
+    }
+})
+console.log('succes');
