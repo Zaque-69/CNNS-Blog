e303b4a58a66b4f885f2591681cd864c98936a92
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-11-05 05:25:42 +0900

    Update filter.js

--- system/static/js/filter.js
+++ system/static/js/filter.js
@@ -1,9 +1,10 @@
-const listOfClasses = ['IXi1', 'Xi1', 'XIi1', 'XIIi1', 'anunt']; list = [];
+const listOfClasses = ['Ve', 'VIIIe', 'IXi2', 'Xi1', 'XIs', 'anunt']; list = [];
     fetch('../static/js/main.json').then(response => response.json()).then(data => {
         for(let i = 0; i < listOfClasses.length; i++) {
             list.push(eval(`data.${listOfClasses[i]}`));
         }
     });
+
 document.getElementById('filterButton').addEventListener('click', function(){
     for (var i = 0; i < list.length; i++) {
         document.querySelectorAll('.' + list[i]).forEach((e) =>{
