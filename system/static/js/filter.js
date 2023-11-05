bcf00c0ad4a383cb119e74d23986a451d0293484
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-11-05 18:57:27 +0200

    Update filter.js

--- system/static/js/filter.js
+++ system/static/js/filter.js
@@ -1,15 +1,18 @@
-const listOfClasses = ['Ve', 'VIIIe', 'IXi2', 'Xi1', 'XIs', 'anunt']; list = [];
+try{
+    const listOfClasses = ['Ve', 'VIIIe', 'IXi2', 'Xi1', 'XIs', 'anunt']; list = [];
     fetch('../static/js/main.json').then(response => response.json()).then(data => {
         for(let i = 0; i < listOfClasses.length; i++) {
             list.push(eval(`data.${listOfClasses[i]}`));
         }
     });
 
-document.getElementById('filterButton').addEventListener('click', function(){
-    for (var i = 0; i < list.length; i++) {
-        document.querySelectorAll('.' + list[i]).forEach((e) =>{
-            if ( list[i] == document.querySelector('select').value ) e.style.display = 'block';
-            else e.style.display = 'none';
-        })
-    }
-})
+    document.getElementById('filterButton').addEventListener('click', function(){
+        for (var i = 0; i < list.length; i++) {
+            document.querySelectorAll('.' + list[i]).forEach((e) =>{
+                if ( list[i] == document.querySelector('select').value ) e.style.display = 'block';
+                else e.style.display = 'none';
+            })
+        }
+    })
+}
+catch{}
