c21bb40e2c0176b5378db0238a28ab1048ed4447
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-11-07 00:28:34 +0200

    Update settings.py

--- log/settings.py
+++ log/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-56%r(1+ek=_9(mrs4e=1bcg2dmr#eg85p4p@55tk_8pon5%+us'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
