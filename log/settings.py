04d34542dea7c3591d837c52578b4e3616cb836c
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-11-07 10:58:29 +0200

    Update settings.py

--- log/settings.py
+++ log/settings.py
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms', "crispy_bootstrap5",
-
 ]
 
 MIDDLEWARE = [
@@ -133,7 +131,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 
 LOGIN_URL = 'log'
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-
-CRISPY_TEMPLATE_PACK = "bootstrap5"
