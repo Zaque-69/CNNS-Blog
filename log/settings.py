9082c2ec626241e7723829fff2d0a41f9c431473
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-11-08 20:53:21 +0200

    Update settings.py

--- log/settings.py
+++ log/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
-
 import environ
 
 env = environ.Env()
