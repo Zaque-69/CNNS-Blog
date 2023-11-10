7d7aab6ffb179f33e5da53f1b77080d0b18609a1
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-11-11 00:54:07 +0200

    Delete system/static directory

--- system/static/js/typeit.js
+++
@@ -1,8 +0,0 @@
-const comments = ["Colegiul National Nichita Stanescu - Ploiesti", 
-	"Only with an invitation code you can acces the website !",
-	"Welcome / Bun venit ! "]
-new TypeIt(".firstType", {
-	strings: comments[Math.floor(Math.random() * comments.length)],
-	speed: 75,
-	loop: false,
-  }).go();
