a2d681d59fada5e548ac75c96520ed212ce732c1
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-11-14 11:33:18 +0200

    Update models.py

--- system/models.py
+++ system/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.TextField()
+    image = models.TextField(blank=True, null = True)
 
     def __str__(self):
         return self.title
@@ -22,7 +22,7 @@ class Comment(models.Model):
     class4post = models.CharField(max_length=100)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.TextField()
+    image = models.TextField(blank=True, null = True)
     #ImageField(blank=True, null=True, upload_to='commentsImages')
 
     def __str__(self) : return self.idComment
