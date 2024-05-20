b5afde4e7b8ca03d104ba7e30ef40bb45f62b319
Z4que <114612941+Zaque-69@users.noreply.github.com>
2024-05-20 22:07:18 +0300

    Delete localDATABASE.bat

--- localDATABASE.bat
+++
@@ -1,5 +0,0 @@
-REM USE RUN THIS FILE ONLY WHEN YOU MODIFY loc/settings.py AND YOU BRING BACK LOCAL DATABASE 
-REM DELETE MODELS MIGRATIONS FROM EVERY APP AND OLD DATABASE
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver
